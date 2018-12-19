4d00949c6d8adea3e287d2b0f9e979c1761fc661
Alexandr Popovich <alexandr.popovih@pushapp.ru>
2018-12-19 13:23:10 +0300

    add bottom navigation

--- app/src/main/java/com/aleksdark/weatherapp/MainActivity.kt
+++ app/src/main/java/com/aleksdark/weatherapp/MainActivity.kt
@@ -2,11 +2,38 @@ package com.aleksdark.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_item_paris.setOnClickListener {
+            showToast("Paris")
+        }
+
+        bottom_item_london.setOnClickListener {
+            showToast("London")
+        }
+
+        bottom_item_newyork.setOnClickListener {
+            showToast("New-York")
+        }
+
+        bottom_item_tokyo.setOnClickListener {
+            showToast("Tokyo")
+        }
+
+        bottom_item_current.setOnClickListener {
+            showToast("Current")
+        }
+
+    }
+
+    fun showToast(msg : String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
